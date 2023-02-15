5db1d64f2c21f8b2be5bb8ede13e2a8b803ace18
lolisnotit <goodgameeba@gmail.com>
2023-02-15 23:34:58 +0900

    User class

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "USER")
+public class user implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+
+}
